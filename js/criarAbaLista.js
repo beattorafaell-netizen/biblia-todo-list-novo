fc1972601f3131edf2fb0a1ba7b3bac80777d553
beattorafaell-natizen <beattorafaell@gmail.com>
2026-05-15 00:03:44 -0300

    agora, para cirar uma lista, é necessario criar um titulo para ela

--- js/criarAbaLista.js
+++ js/criarAbaLista.js
@@ -1,52 +1,75 @@
 
+
 function criarLista() {
 
     const divLista = document.createElement('div')
 
-    const labelLista = document.createElement('label')
-    labelLista.textContent = "Digite aqui um item da sua lista:"
-    labelLista.classList.add('itemListaBusca')
+    const InputTitutoLista = document.createElement('input')
+
+    const btnInputTitutoLista = document.createElement('button')
+
+    btnInputTitutoLista.addEventListener('click', () => {
+
+        InputTitutoLista.style.display = 'none'
+        btnInputTitutoLista.style.display = 'none'
+
+        const titulo = document.createElement('h1')
+        titulo.classList.add('tituloLista')
+        titulo.textContent = InputTitutoLista.value
+
+        const labelLista = document.createElement('label')
+        labelLista.textContent = "Digite aqui um item da sua lista:"
+        labelLista.classList.add('itemListaBusca')
 
 
+        const lista = document.createElement('input')
+        lista.type = 'text'
+        lista.classList.add('itemListaBusca')
 
-    const lista = document.createElement('input')
-    lista.type = 'text'
-    lista.classList.add('itemListaBusca')
-
-    const btnAddLista = document.createElement('button')
-    btnAddLista.textContent = "Enviar"
-
-
-    btnAddLista.addEventListener('click', () => {
-        const itemDaLista = document.createElement('input')
-        itemDaLista.classList.add("itemDaLista")
-        itemDaLista.type = 'checkbox'
+        const btnAddLista = document.createElement('button')
+        btnAddLista.textContent = "Enviar"
 
 
-        const itemDaListaSpan = document.createElement('span')
-        itemDaLista.classList.add('itemDaListaSpan')
+        btnAddLista.addEventListener('click', () => {
+
+            
+
+            
+
+            const itemDaLista = document.createElement('input')
+            itemDaLista.classList.add("itemDaLista")
+            itemDaLista.type = 'checkbox'
+
+
+            const itemDaListaSpan = document.createElement('span')
+            itemDaLista.classList.add('itemDaListaSpan')
+            
+            itemDaListaSpan.innerHTML = lista.value
+
+            const btnEditarItemLista = document.createElement('button')
+            btnEditarItemLista.classList.add('btnEditarItemLista')
+
+            btnEditarItemLista.addEventListener('click', () => {
+                const novoItemEditado = prompt("Qual o novo nome?")
+                itemDaListaSpan.innerHTML = novoItemEditado
+
+            })
         
-        itemDaListaSpan.innerHTML = lista.value
-
-        const btnEditarItemLista = document.createElement('button')
-        btnEditarItemLista.classList.add('btnEditarItemLista')
-
-        btnEditarItemLista.addEventListener('click', () => {
-            const novoItemEditado = prompt("Qual o novo nome?")
-             itemDaListaSpan.innerHTML = novoItemEditado
-
+            
+            divLista.append(itemDaLista)
+            divLista.append(itemDaListaSpan)
+            divLista.append(btnEditarItemLista)
+            
         })
-    
+        divLista.append(titulo)
+        divLista.append(labelLista)
+        divLista.append(lista)
+        divLista.append(btnAddLista)
 
-        divLista.append(itemDaLista)
-        divLista.append(itemDaListaSpan)
-        divLista.append(btnEditarItemLista)
     })
 
-    divLista.append(labelLista)
-    divLista.append(lista)
-    divLista.append(btnAddLista)
-
+    divLista.append(InputTitutoLista)    
+    divLista.append(btnInputTitutoLista)
     return divLista
 
 }
