f81b10ca3542315775505ddc57adf88d45bf8091
beattorafaell-natizen <beattorafaell@gmail.com>
2026-05-14 22:25:30 -0300

    campo de titulo e logica para adicionar o titulo na div textearea

--- js/criarAbaTextearea.js
+++ js/criarAbaTextearea.js
@@ -1,11 +1,39 @@
-function conteudoTextearea() {
-    const divTexatearea = document.createElement('div')
-    const texteareaValor = document.createElement('textarea')
 
+
+function conteudoTextearea() {
+
+    const divTexatearea = document.createElement('div')
+    divTexatearea.classList.add('divTextearea')
+
+    const labelTituloTextearea = document.createElement('label')
+    labelTituloTextearea.classList.add('labelTituloTextearea')
+    labelTituloTextearea.textContent = "Digite aqui seu titulo da sessão"
+
+    
+    
+    const inputTituloTextearea = document.createElement('input')
+    inputTituloTextearea.classList.add('inputTituloTextearea')
+
+    inputTituloTextearea.addEventListener('click', () => {
+        tituloTextearea.textContent = inputTituloTextearea.value
+        labelTituloTextearea.style.display = 'none'
+        labelTituloTextearea.style.display = 'none'
+    })
+
+    
+    const tituloTextearea = document.createElement('h2')
+    tituloTextearea.classList.add('tituloTextearea')
+
+
+    const texteareaValor = document.createElement('textarea')
+    texteareaValor.classList.add('texteareaValor')
 
 
     const btnTesteareaAparecer = document.createElement('button')
-    btnTesteareaAparecer.innerHTML = "alsdnflasdn"
+    btnTesteareaAparecer.classList.add('btnTesteareaAparecer')
+    btnTesteareaAparecer.innerHTML = "Salvar"
+
+
     btnTesteareaAparecer.addEventListener('click', () => {
         texteareaValor.style.display = 'none'
         btnTesteareaAparecer.style.display = 'none'
@@ -13,14 +41,16 @@ function conteudoTextearea() {
 
         const pTextearea = document.createElement('p')
         pTextearea.innerHTML = texteareaValor.value
+
+
         divTexatearea.append(pTextearea)
     })
 
 
-
-
+    divTexatearea.append(labelTituloTextearea)
+    divTexatearea.append(inputTituloTextearea)
+    divTexatearea.append(tituloTextearea)
     divTexatearea.append(texteareaValor)
-
     divTexatearea.append(btnTesteareaAparecer)
 
     return divTexatearea
